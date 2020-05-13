2512ef170e9685cba831a44030673ea4ccef20f2
arviinnd <arvindn.iitkgp@gmail.com>
2020-05-13 16:02:24 +0530

    Bug fixing for random quote

--- src/App.js
+++ src/App.js
@@ -14,7 +14,14 @@ class QuoteGenerator extends React.Component {
       randomQuote: null,
       author:''
     };
-    this.randomQuoteNumber = this.randomQuoteNumber.bind(this);
+  }
+  randomQuoteNumber=()=> {
+    const randNum = Math.floor(Math.random() * this.state.quotes.length);
+    const randomQuote = this.state.quotes[randNum];
+    this.setState({
+      randomQuote: randomQuote.quote,
+      author: randomQuote.author
+    });
   }
   async componentDidMount() {
     const url =
@@ -22,40 +29,26 @@ class QuoteGenerator extends React.Component {
     const response = await fetch(url);
     const data = await response.json();
     this.setState({ quotes: data["quotes"], loading: false });
-    console.log(this.state.quotes.length);
     window.addEventListener('load', this.randomQuoteNumber);
   }
   
-  randomQuoteNumber() {
-    const randNum = Math.floor(Math.random() * this.state.quotes.length);
-    // console.log(randNum)
-    const randomQuote = this.state.quotes[randNum];
-    // console.log(randomQuote)
-    this.setState({
-      randomQuote: randomQuote.quote,
-      randomAuthor: randomQuote.author
-    });
-  }
+  
  
   
   
   render() {
-    if (this.state.loading) {
-      return <div>loading...</div>
-    }
-    
     return (
       <div id="wrapper">
           <div className="header">Random Quote Generator</div>
           <div className="App" id="quote-box">
               <div className="grid">
-              {this.props.randomQuote}
+            
                 
                 <div className="text" id="text"><FaQuoteLeft id="left-quote" />{this.state.randomQuote}</div>
               </div>
-              <div id="author">- {this.state.randomAuthor}</div>
+              <div id="author">- {this.state.author}</div>
               <div className="button-anchor-tag">
-                  <a href="http://twitter.com/intent/tweet?hashtags=quotes&text=" id="tweet-quote"><FaTwitterSquare /></a>
+                  <a href={`http://twitter.com/intent/tweet?hashtags=quotes&text="${this.state.randomQuote}--${this.state.author} `}   id="tweet-quote"><FaTwitterSquare /></a>
                   <button id="new-quote" onClick={this.randomQuoteNumber}>New Quote</button>
               </div>
           </div>
@@ -67,13 +60,3 @@ class QuoteGenerator extends React.Component {
 }
 
 export default QuoteGenerator;
-
-
-    // if (this.state.) {
-    //   return <div>There is no person yet receive</div>
-    // }
-    // const loadquote=document.addEventListener("load",this.randomQuoteNumber())
-    // const url="http://twitter.com/intent/tweet?hashtags=quotes&text=" + '"' + props.quote + '"' + " " + props.author;"
-    
-    // const url="http://twitter.com/intent/tweet?hashtags=quotes&text=" + '"' + {this.state.randomQuote !==null && this.state.randomQuote.quote} + '"' + " " + {this.state.randomQuote !==null && this.state.randomQuote.author};
-    
